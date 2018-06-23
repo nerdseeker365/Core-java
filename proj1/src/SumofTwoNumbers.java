468d5645bf030cd75a4941b213f516992f32db7a
karra sankar <karrasankar158@gmail.com>
2018-06-23 21:52:15 +0530

    sum of two numbers

---
+++ proj1/src/SumofTwoNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SumofTwoNumbers {
+	public static void main(String[] args) {
+	int a=0;
+	int b=0;
+	int sum =0;
+	Scanner sc=null;
+	sc=new Scanner(System.in);
+	System.out.print("Enetr a value: ");
+	a=sc.nextInt();
+	System.out.print("Enetr b value: ");
+	b=sc.nextInt();
+	System.out.println("Total: ");
+	sum=a+b;
+	System.out.println(sum);
+	
+	
+	
+	
+	
+	}
+}
