891f73111f8cfa99f054c3411695328fa50eafc5
karra sankar <karrasankar158@gmail.com>
2018-06-24 10:09:19 +0530

    method and argument program

---
+++ proj1/src/MethodsAndArguments.java
@@ -0,0 +1,11 @@
+
+public class MethodsAndArguments {
+	static void m4(int a, int b) {//() variables declared in brackets are called parameters
+		 int c=a+b;
+		 System.out.print("result= "+c);
+	 }
+	 public static void main(String[] args) {
+		 m4(10,20);//arguments order and type,type means type of values.
+	 }
+	 
+}
