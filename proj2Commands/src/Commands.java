99e796635dba1dff55c25ba9d29c42a2166c9577
karra sankar <karrasankar158@gmail.com>
2018-06-24 13:23:37 +0530

    JAVA COMMANDS

---
+++ proj2Commands/src/Commands.java
@@ -0,0 +1,36 @@
+/**
+ * this is java programming
+ * java version is 1.9.0 version
+ * jre adding to the eclipse
+ * this is document command 
+ */
+
+/*
+ * is used to multiple line command
+ * is used in more than one line
+ */
+
+
+
+
+
+
+
+
+public class Commands {//this is class name
+	int a=0;
+	int getNumber() {//is used to single line command
+		a=10;
+		System.out.println("display Result: "+a);   
+		return a;
+		
+	}
+
+	public static void main(String[] args) {
+		Commands cd=null;
+		cd=new Commands(); 
+		cd.getNumber();
+
+	}
+
+}
