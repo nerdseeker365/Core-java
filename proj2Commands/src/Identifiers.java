5df076b87a231d9e89a267c14344482e2a535a1c
karra sankar <karrasankar158@gmail.com>
2018-06-24 13:42:25 +0530

    Identifiers

---
+++ proj2Commands/src/Identifiers.java
@@ -0,0 +1,14 @@
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		System.out.println("identifiers are:\nalphabets\nnumbers 0-09\nspecial characters _,$");
+         System.out.println("identifier shoud not start with number");
+         System.out.println("Special characters only allowed _,$");
+         System.out.println( "identifiers shoud not contain space in the middle of word,if we want provide+"
+         		+ "use connector_ unserscore");
+         System.out.println("identifires are case sensitive");
+         System.out.println("keywords are can't use user defined identifier");
+	}
+
+}
