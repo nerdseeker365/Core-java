918ef1f02d7ba61a762f146e6136b503effd78fe
karra sankar <karrasankar09@gmail.com>
2018-06-24 18:08:13 +0530

    Simple star print 5lines .increment each line one

---
+++ Patterns/src/SamplePattern.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class SamplePattern {
+	 public static void printStars(int n) {
+	   	        int i, j;
+		        for(i=0; i<n; i++)  {
+	      
+	 	            for(j=0; j<=i; j++)   {
+	         	                // printing stars
+	                System.out.print("* ");
+	            }
+	 
+	            // ending line after each row
+	            System.out.println();
+	        }
+	   }
+           public static void main(String args[])   {
+	    Scanner sc=null;
+	    int n = 0;
+	    sc=new Scanner(System.in);
+	    System.out.print("Enter n value:");
+	    n=sc.nextInt();
+	    printStars(n);
+	    }
+	}
+
